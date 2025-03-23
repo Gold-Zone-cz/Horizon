8b10cea4f38f95e5e3c5a7bc60f02084369bb590
jogg15 <jogger.cztechnic@gmail.com>
2025-03-23 18:16:08 +0100

    fix

--- src/main/java/cz/goldzone/horizon/FillTab.java
+++ src/main/java/cz/goldzone/horizon/FillTab.java
@@ -3,29 +3,39 @@ package cz.goldzone.horizon;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.configuration.file.FileConfiguration;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+
 
 public class FillTab implements TabCompleter {
 
-    private final List<String> nexusArguments = List.of("reload");
+    private final List<String> horizonArguments = List.of("reload");
     private final List<String> playerWarpsArguments = List.of("create", "delete", "list");
-    private final List<String> warpArguments = List.of("reload", "setwarp", "delwarp");
+
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        List<String> suggestions = new ArrayList<>();
+
         if (args.length == 1) {
             if (command.getName().equalsIgnoreCase("horizon") && sender.hasPermission("horizon.admin.reload")) {
-                return nexusArguments;
+                return horizonArguments;
             }
             if (command.getName().equalsIgnoreCase("pwarp")) {
                 return playerWarpsArguments;
             }
             if (command.getName().equalsIgnoreCase("warp") && sender.hasPermission("horizon.warp.admin")) {
-                return warpArguments;
+                FileConfiguration warpsConfig = Main.getConfigManager().getConfig("warps.yml");
+                if (warpsConfig != null) {
+                    Set<String> warpKeys = warpsConfig.getKeys(false);
+                    suggestions.addAll(warpKeys);
+                }
             }
         }
 
-        return null;
+        return suggestions.isEmpty() ? null : suggestions;
     }
 }
