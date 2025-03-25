4e169eea7f67471c97338162ab6dbaa8e05665cf
jogg15 <jogger.cztechnic@gmail.com>
2025-03-25 20:04:09 +0100

    changes

--- src/main/java/cz/goldzone/horizon/Main.java
+++ src/main/java/cz/goldzone/horizon/Main.java
@@ -3,19 +3,30 @@ package cz.goldzone.horizon;
 import com.google.gson.Gson;
 import cz.goldzone.horizon.commands.HorizonCommand;
 import cz.goldzone.horizon.commands.PlayerWarpsCommand;
+import cz.goldzone.horizon.commands.admin.ItemCommand;
+import cz.goldzone.horizon.commands.economy.BalanceCommand;
+import cz.goldzone.horizon.commands.global.CraftCommand;
+import cz.goldzone.horizon.commands.global.EnderChestCommand;
 import cz.goldzone.horizon.commands.home.DelHomeCommand;
 import cz.goldzone.horizon.commands.home.HomeCommand;
 import cz.goldzone.horizon.commands.home.HomeListCommand;
 import cz.goldzone.horizon.commands.home.SetHomeCommand;
+import cz.goldzone.horizon.commands.player.TpaAcceptCommand;
+import cz.goldzone.horizon.commands.player.TpaCommand;
+import cz.goldzone.horizon.commands.player.TpaDenyCommand;
 import cz.goldzone.horizon.commands.warp.WarpsListCommand;
 import cz.goldzone.horizon.commands.warp.DelWarpCommand;
 import cz.goldzone.horizon.commands.warp.SetWarpCommand;
 import cz.goldzone.horizon.commands.warp.WarpCommand;
 import cz.goldzone.horizon.managers.HomesManager;
+import cz.goldzone.horizon.managers.MoneyManager;
+import cz.goldzone.horizon.managers.TeleportManager;
 import lombok.Getter;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
 import cz.goldzone.horizon.managers.ConfigManager;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public final class Main extends JavaPlugin {
@@ -33,24 +44,36 @@ public final class Main extends JavaPlugin {
         configManager = new ConfigManager(this);
 
         registerCommands();
+
         HomesManager.createHomesTable();
+        MoneyManager.createBalanceTable();
 
         getLogger().info("Plugin successfully started with all configuration files loaded!");
     }
 
     private void registerCommands() {
-        Map.of(
-                "horizon", new HorizonCommand(),
-                "warp", new WarpCommand(),
-                "warps", new WarpsListCommand(),
-                "setwarp", new SetWarpCommand(),
-                "delwarp", new DelWarpCommand(),
-                "pwarp", new PlayerWarpsCommand(),
-                "sethome", new SetHomeCommand(),
-                "delhome", new DelHomeCommand(),
-                "home", new HomeCommand(),
-                "homes", new HomeListCommand()
-        ).forEach((cmd, executor) -> {
+        TeleportManager teleportManager = new TeleportManager();
+
+        Map<String, CommandExecutor> commands = new HashMap<>();
+        commands.put("horizon", new HorizonCommand());
+        commands.put("warp", new WarpCommand());
+        commands.put("warps", new WarpsListCommand());
+        commands.put("setwarp", new SetWarpCommand());
+        commands.put("delwarp", new DelWarpCommand());
+        commands.put("pwarp", new PlayerWarpsCommand());
+        commands.put("sethome", new SetHomeCommand());
+        commands.put("delhome", new DelHomeCommand());
+        commands.put("home", new HomeCommand());
+        commands.put("homes", new HomeListCommand());
+        commands.put("wb", new CraftCommand());
+        commands.put("enderchest", new EnderChestCommand());
+        commands.put("tpa", new TpaCommand(teleportManager));
+        commands.put("tpaccept", new TpaAcceptCommand(teleportManager));
+        commands.put("tpdeny", new TpaDenyCommand(teleportManager));
+        commands.put("i", new ItemCommand());
+        commands.put("balance", new BalanceCommand());
+
+        commands.forEach((cmd, executor) -> {
             if (getCommand(cmd) != null) {
                 getCommand(cmd).setExecutor(executor);
                 getCommand(cmd).setTabCompleter(new FillTab());
--- src/main/java/cz/goldzone/horizon/commands/warp/WarpsListCommand.java
+++ src/main/java/cz/goldzone/horizon/commands/warp/WarpsListCommand.java
@@ -2,6 +2,10 @@ package cz.goldzone.horizon.commands.warp;
 
 import cz.goldzone.horizon.Main;
 import cz.goldzone.neuron.shared.Lang;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -28,10 +32,13 @@ public class WarpsListCommand implements CommandExecutor {
 
         Set<String> warpKeys = warpsConfig.getKeys(false);
         if (warpKeys.isEmpty()) {
-            player.sendMessage("<#333333>【 <red>No warps available. <#333333>】");
+            player.sendMessage("<dark_gray>【 <red>No warps available. <dark_gray>】");
         } else {
             for (String key : warpKeys) {
-                player.sendMessage("<#333333>【 <red>" + key + " <#333333>】");
+                TextComponent warpText = new TextComponent("<dark_gray>【 <red>" + key + " <dark_gray>】");
+                warpText.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("<gray>Click to insert into chat").create()));
+                warpText.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/warp " + key));
+                player.spigot().sendMessage(warpText);
             }
         }
 
