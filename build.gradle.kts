c562deffe1ba9d9e91b34880a0d932d552799d06
Kohout157 <kohout157@seznam.cz>
2025-04-14 15:11:24 +0200

    groovy to kts

---
+++ build.gradle.kts
@@ -0,0 +1,97 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    id("java")
+    id("com.gradleup.shadow") version "8.3.6"
+}
+
+group = "cz.goldzone"
+description = "Horizon"
+version = "1.0.3"
+
+java.toolchain {
+    languageVersion = JavaLanguageVersion.of(21)
+    vendor = JvmVendorSpec.ADOPTIUM
+}
+
+repositories {
+    mavenCentral()
+
+    maven {
+        url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    }
+
+    maven {
+        url = uri("https://repo.codemc.io/repository/maven-snapshots/")
+    }
+
+    maven {
+        url = uri("https://oss.sonatype.org/content/groups/public/")
+    }
+
+    maven {
+        url = uri("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+    }
+
+    maven {
+        url = uri("https://jitpack.io")
+    }
+
+    maven {
+        url = uri("https://repo.gold-zone.cz/releases")
+    }
+
+    maven {
+        url = uri("https://repo.gold-zone.cz/private")
+
+        credentials {
+            username = (project.findProperty("goldzoneRepo.username") ?: System.getenv("GOLDZONE_REPO_USERNAME")) as String?
+            password = (project.findProperty("goldzoneRepo.password") ?: System.getenv("GOLDZONE_REPO_PASSWORD")) as String?
+        }
+
+        authentication {
+            create<BasicAuthentication>("basic")
+        }
+    }
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.21.5-R0.1-SNAPSHOT")
+    compileOnly("cz.goldzone:neuron-spigot:1.0.0-SNAPSHOT")
+
+    implementation("net.md-5:bungeecord-chat:1.21-R0.2")
+    implementation("com.github.cryptomorin:XSeries:13.2.0")
+    implementation("dev.digitality:digitalgui:1.1.2")
+    implementation("dev.digitality:digitalconfig:1.2.0")
+
+    compileOnly("me.clip:placeholderapi:2.11.6")
+    compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
+
+    implementation("org.projectlombok:lombok:1.18.38")
+    annotationProcessor("org.projectlombok:lombok:1.18.38")
+}
+
+tasks {
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+    }
+
+    withType<ShadowJar> {
+        minimize {
+            exclude(dependency("dev.digitality:digitalgui"))
+        }
+
+        relocate("dev.digitality.digitalgui", "cz.goldzone.horizon.api.digitalgui")
+        relocate("dev.digitality.digitalconfig", "cz.goldzone.horizon.api.digitalconfig")
+
+        archiveFileName = "${project.name}.jar"
+    }
+
+    processResources {
+        filteringCharset = "UTF-8"
+
+        filesMatching("plugin.yml") {
+            expand(hashMapOf("version" to project.version))
+        }
+    }
+}
